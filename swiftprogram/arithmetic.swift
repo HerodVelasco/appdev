548fa61072e6663221f7929e25a5b288e1733f93
HerodVelasco <120539550+HerodVelasco@users.noreply.github.com>
2022-12-14 11:34:23 +0800

    Added difference and quotient in arithmetic.swift

--- swiftprogram/arithmetic.swift
+++ swiftprogram/arithmetic.swift
@@ -12,4 +12,9 @@ if let num2 = Int(readLine()!){
 
 print("The product: \(product)")
 
-
+var num1 = 12
+var num2 = 15
+var difference = num2 - num1
+var quotient = num1 / num2
+print("The difference of num2 and num1 is \(difference)")
+print("The quotient of num1 and num2 is \(quotient)") 
